b5146c7c771d7a73a23aee122e87082489c8ad84
alephG <solimlegris@gmail.com>
2022-01-05 00:14:53 -0500

    new tests

--- test/test_clean.py
+++ test/test_clean.py
@@ -10,11 +10,26 @@ import src.run_simulations as srs
 class CleanTest(unittest.TestCase):
     def setUp(self):
         self.entries = []
-        self.entries.append(os.path.join(parentdir, 'test','fixtures', 'test1_config.json'))
-   
+        self.entries.append(os.path.join(parentdir, 'test', 'fixtures', 'test1_config.json'))
+        self.entries.append(os.path.join(parentdir, 'test', 'fixtures', 'test2_config.json'))
+        self.entries.append(os.path.join(parentdir, 'test', 'fixtures', 'test3_config.json'))
+    
+    # Test 4
+    def test_4(self):
+        print('\nRunning test 4: Test conv net on most difficult categories from experiment 1')
+        config, data_dir, exp_name, _ = sgd.get_configuration(self.entries[2])
+        path_mf, path_cat, macro_fname = sgd.create_dirstruct(data_dir, exp_name)
+        sgd.generate_binary_custom(**config, path_mf=path_mf, macro_fname=macro_fname, path_cat=path_cat)
+        srs.main(config_fname=self.entries[2])
+
+    # Test 3
+    def test_3(self):
+        print('\nRunning test 3: Verify that conv net is working/learning')
+        srs.main(config_fname=self.entries[1])
+
     # Test 2
     def test_2(self):
-        print('\nRunning test 1: Verify that dataset simulator is functional')
+        print('\nRunning test 2: Verify that dataset simulator is functional')
         srs.main(config_fname=self.entries[0])
     
     # Test 1 
