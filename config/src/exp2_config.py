74a3b14b7858b6fa7c6f34ecc9e7c76d8b5d182f
alephG <solimlegris@gmail.com>
2022-01-06 23:56:39 -0500

    added new simulation parameters

--- config/src/exp2_config.py
+++ config/src/exp2_config.py
@@ -22,8 +22,8 @@ def sim_config():
     # Set sim parameters
     sim_params = OrderedDict({'encoder_out_name': 'lin1_encoder', 'train_ratio': 0.8, 'AE_epochs': 15, 
                               'AE_batch_size': 8, 'noise_factor': 0.1, 'AE_lr': 10e-5,'AE_wd': 10e-5, 
-                              'class_epochs': 15, 'class_batch_size': 8, 'class_lr': 10e-2, 
-                              'class_wd': 10e-3, 'inplace_noise': True, 'verbose': False, 'metric':'cosine'})
+                              'AE_thresh': 0.001, 'AE_patience': 3, 'class_epochs': 15, 'class_batch_size': 8, 'class_lr': 10e-2, 
+                              'class_wd': 10e-3, 'training': 'early_stop', 'inplace_noise': True, 'save_model': True, 'verbose': False, 'metric':'cosine'})
     
     return layer_params, sim_params
 
