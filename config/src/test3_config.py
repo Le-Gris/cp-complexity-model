dba31e4311bc0263b8252669b46dbe822d6f246e
alephG <solimlegris@gmail.com>
2022-01-16 22:32:39 -0500

    new config settings

--- config/src/test3_config.py
+++ config/src/test3_config.py
@@ -22,8 +22,8 @@ def sim_config():
     # Set sim parameters
     sim_params = OrderedDict({'encoder_out_name': 'lin1_encoder', 'train_ratio': 0.8, 'AE_epochs': 50, 
                               'AE_batch_size': 8, 'noise_factor': 0.1, 'AE_lr': 10e-5,'AE_wd': 10e-5, 
-                              'AE_thresh': 0.001, 'AE_patience': 3, 'class_epochs': 50, 'class_batch_size': 8, 'class_lr': 10e-2, 
-                              'class_wd': 10e-3, 'class_monitor': 'acc', 'class_thresh': 85, 'training': 'early_stop', 
+                              'AE_thresh': 0.001, 'AE_patience': 5, 'class_epochs': 50, 'class_batch_size': 8, 'class_lr': 10e-2, 
+                              'class_wd': 10e-3, 'class_monitor': 'loss', 'class_thresh': 0.001, 'training': 'early_stop', 
                               'inplace_noise': True, 'save_model': True, 'metric':'cosine', 'verbose': False})
     
     return layer_params, sim_params
