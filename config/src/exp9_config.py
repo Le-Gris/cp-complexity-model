8490f00760bda43c9d30a2747e08bfe2e5dac3ab
AlephG <solimlegris@gmail.com>
2022-03-05 00:43:30 -0500

    exp9 updates

--- config/src/exp9_config.py
+++ config/src/exp9_config.py
@@ -1,6 +1,9 @@
 """
-Configuration file for simulations with benchmark stimuli. Constraints on autoencoder (dimensionality reduction). 
-Saving weightings from inner layer. Inner representation CP measurements are linear. Activation is sigmoidal all the way through.
+Configuration file for simulations with benchmark stimuli. 
+Constraints on autoencoder (dimensionality reduction). 
+Saving weightings from inner layer. 
+Inner representation CP measurements are non-linear. 
+Activation is sigmoidal all the way through.
 """
 import json
 import argparse
@@ -23,11 +26,11 @@ def sim_config():
                     'classifier_out': 2}
 
     # Set sim parameters
-    sim_params = OrderedDict({'train_ratio': 0.7, 'AE_epochs': 50, 
+    sim_params = OrderedDict({'train_ratio': 0.7, 'AE_epochs': 100, 
                               'AE_batch_size': 8, 'noise_factor': 0.1, 'AE_lr': 10e-3,'AE_wd': 10e-5, 
-                              'AE_thresh': 0.001, 'AE_patience': 5, 'class_epochs': 50, 'class_batch_size': 8, 'class_lr': 10e-2, 
+                              'AE_thresh': 0.001, 'AE_patience': 5, 'class_epochs': 100, 'class_batch_size': 8, 'class_lr': 10e-2, 
                               'class_wd': 10e-3, 'class_monitor': 'loss', 'class_thresh': 0.001, 'training': 'early_stop', 
-                              'inplace_noise': True, 'rep_type': 'lin', 'save_model': True, 'metric':'cosine', 'verbose': False})
+                              'inplace_noise': True, 'rep_type': 'act', 'save_model': True, 'metric':'cosine', 'verbose': False})
     
     return layer_params, sim_params
 
@@ -37,35 +40,35 @@ def main(**kwargs):
     save_fname = kwargs['save_fname']
 
     # Experiment 1 config
-    exp5 = {}
+    exp9 = {}
     
     # Data set
-    exp5['dataset'] = {}
-    exp5['sim'] = {}
-    exp5['mode'] = 'benchmark'
-    exp5['model'] = 'nn-sig'
-    exp5['exp_name'] = 'exp9'
-    exp5['dataset_dir'] = osp.abspath(osp.join(Path(__file__).parent, '..', '..', 'data', 'benchmark_stimuli', 'N8'))
-    exp5['save_dir'] = osp.abspath(osp.join(Path(__file__).parent, '..', '..', 'results'))
+    exp9['dataset'] = {}
+    exp9['sim'] = {}
+    exp9['mode'] = 'benchmark'
+    exp9['model'] = 'nn-sig'
+    exp9['exp_name'] = 'exp9'
+    exp9['dataset_dir'] = osp.abspath(osp.join(Path(__file__).parent, '..', '..', 'data', 'benchmark_stimuli', 'N8'))
+    exp9['save_dir'] = osp.abspath(osp.join(Path(__file__).parent, '..', '..', 'results'))
     
     ## Macrofeature parameters
-    exp5['dataset']['size'] = 1000
-    exp5['dataset']['i'] = None
-    exp5['dataset']['k'] = None
-    exp5['dataset']['l'] = None
-    exp5['dataset']['m'] = None
-    exp5['dataset']['s'] = None
-    exp5['dataset']['s_list'] = None
+    exp9['dataset']['size'] = 1000
+    exp9['dataset']['i'] = None
+    exp9['dataset']['k'] = None
+    exp9['dataset']['l'] = None
+    exp9['dataset']['m'] = None
+    exp9['dataset']['s'] = None
+    exp9['dataset']['s_list'] = None
 
     ## Category parameters
     ### In order, each element contains: k, d, pdi, pd
-    exp5['dataset']['custom'] = None 
+    exp9['dataset']['custom'] = None 
     
     # Simulation parameters
-    exp5['sim']['layer_params'], exp5['sim']['sim_params'] = sim_config() 
+    exp9['sim']['layer_params'], exp9['sim']['sim_params'] = sim_config() 
     
     with open(save_fname, 'w') as f:
-        json.dump(exp5, f, indent=3)
+        json.dump(exp9, f, indent=3)
 
 if __name__ == "__main__":
     save_fname = parse_args()
